92d5a1d1bbd3756298b3de0938a18cc81235ac4b
Marco1111998 <96786575+Marco1111998@users.noreply.github.com>
2026-07-06 18:46:37 +0100

    Update streams.sh

--- M3U/streams.sh
+++ M3U/streams.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-# Method 1
+cd M3U
 
-# Update the stream URL of all TVI channels
+# TVI - update the stream URL of TVI
 
-sed -i "s#wmsAuthSign=[^&]*#wmsAuthSign=$(wget -qO- https://services.iol.pt/matrix?userId -o /dev/null)#g" *.m3u8
+sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# CNN Portugal - update the stream URL of CNN Portugal
+
+sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# V+ TVI - update the stream URL of V+ TVI
+
+sed -i "/live_vmais/ c https://video-auth2.iol.pt/live_vmais/live_vmais/edge_servers/vmais-720p/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# TVI Ficção - update the stream URL of TVI Ficção
+
+sed -i "/live_tvi_ficcao/ c https://video-auth1.iol.pt/live_tvi_ficcao/live_tvi_ficcao/edge_servers/tvificcao-720p/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# TVI Reality - update the stream URL of TVI Reality
+
+sed -i "/live_tvi_reality/ c https://video-auth6.iol.pt/live_tvi_reality/live_tvi_reality/edge_servers/tvireality-720_passthrough/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# TVI Internacional - update the stream URL of TVI Internacional
+
+sed -i "/live_tvi_internacional/ c https://video-auth6.iol.pt/live_tvi_internacional/live_tvi_internacional/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+
+# Porto Canal - update the stream URL of Porto Canal
+
+python porto_canal.py && sed -e '/x8egnb8/ {' -e 'r porto_canal.txt' -e 'd' -e '}' -i M3UPT.m3u
 
 exit 0
